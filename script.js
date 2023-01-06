2345c1ed0886c04c4e61dbb5b6e3d913352f722e
djsereno <djsereno91@gmail.com>
2023-01-05 22:42:07 -0800

    Add resize grid button and implement variable grid size

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+const resizeGridButton = document.createElement("button");
+resizeGridButton.type = "button";
+resizeGridButton.innerText = "Resize Grid";
+resizeGridButton.addEventListener("click", resizeGrid);
+document.querySelector("body").append(resizeGridButton);
+
+let gridSize = 50;
+function resizeGrid() {
+  let userInput;
+  do {
+    userInput = +prompt(
+      "Enter the number of squares per side of the new grid (min = 16, max = 100)",
+      gridSize
+    );
+    if (userInput === 0) return;
+  } while (userInput < 16 || userInput > 100 || !userInput);
+
+  gridSize = userInput;
+}
+
 const gridContainer = document.createElement("div");
 gridContainer.setAttribute("class", "grid-container");
-const gridSize = 16;
+const gridDimension = 800;
 
+const cellSize = Math.floor(gridDimension / gridSize);
 for (let row = 0; row < gridSize; row++) {
   let rowContainer = document.createElement("div");
   rowContainer.setAttribute("class", "row-container");
@@ -10,7 +31,8 @@ for (let row = 0; row < gridSize; row++) {
     let cell = document.createElement("div");
     cell.setAttribute("class", "cell");
     cell.id = col;
-    cell.innerText = ""
+    cell.style.minWidth = cellSize + "px";
+    cell.style.minHeight = cellSize + "px";
     rowContainer.append(cell);
   }
   gridContainer.append(rowContainer);
